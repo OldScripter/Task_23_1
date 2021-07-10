d5a6a02eb534a91dd79a986afb99019447864147
OldScripter <Oldscripter@gmail.com>
2021-07-10 15:13:08 +0300

    feature (main) macroses with concat

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 
-#define WEEKDAY(a) \
-if (a == 1) (std::cout << "MONDAY"); \
-else if (a == 2) (std::cout << "TUESDAY"); \
-else if (a == 3) (std::cout << "WEDNESDAY"); \
-else if (a == 4) (std::cout << "THURSDAY"); \
-else if (a == 5) (std::cout << "FRIDAY"); \
-else if (a == 6) (std::cout << "SATURDAY"); \
-else if (a == 7) (std::cout << "SUNDAY"); \
-else std::cerr << "Strange day... Please enter from 1 to 7."; \
+#define WEEKDAY(a) WEEKDAY_##a
+
+#define WEEKDAY_1 "MONDAY"
+#define WEEKDAY_2 "TUESDAY"
+#define WEEKDAY_3 "WEDNESDAY"
+#define WEEKDAY_4 "THURSDAY"
+#define WEEKDAY_5 "FRIDAY"
+#define WEEKDAY_6 "SATURDAY"
+#define WEEKDAY_7 "SUNDAY"
+#define WEEKDAY_N "Strange day..."
+
+#define GET_WEEKDAY(a) \
+if (a == 1) (std::cout << WEEKDAY(1)); \
+else if (a == 2) (std::cout << WEEKDAY(2)); \
+else if (a == 3) (std::cout << WEEKDAY(3)); \
+else if (a == 4) (std::cout << WEEKDAY(4)); \
+else if (a == 5) (std::cout << WEEKDAY(5)); \
+else if (a == 6) (std::cout << WEEKDAY(6)); \
+else if (a == 7) (std::cout << WEEKDAY(7)); \
+else (std::cerr << WEEKDAY(N));
 
 int main() {
+
     std::cout << "Please enter the day number from 1 to 7:\n";
-    int day;
-    std::cin >> day;
-    WEEKDAY(day);
+    int a;
+    std::cin >> a;
+
+    GET_WEEKDAY(a);
+
     return 0;
 }
